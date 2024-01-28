7ac87bb823882ff30fa5520bb9389740adb80b82
Ismail Saleh <centriu78@gmail.com>
2024-01-28 23:50:16 +0200

    a

--- src/persistent-data/db.js
+++ src/persistent-data/db.js
@@ -3,5 +3,5 @@ import Dexie from "dexie";
 export const db = new Dexie("mainDB");
 db.version(2).stores({
   settings:
-    "timerDuration, restDuration, wantsMaxWhenTimerDone, wantsMinWhenTimerStart",
+    "workDuration, restDuration, wantsMaxWhenTimerDone, wantsMinWhenTimerStart",
 });
--- src/views/ViewSettings.vue
+++ src/views/ViewSettings.vue
@@ -1,7 +1,26 @@
 <template>
-  <div></div>
+  <v-container>
+    <v-text-field
+      label="Work duration (s)"
+      v-model="workDuration"
+    ></v-text-field>
+    <v-text-field
+      label="Rest duration (s)"
+      v-model="restDuration"
+    ></v-text-field>
+  </v-container>
 </template>
 <script>
-export default {};
+import { SettingsController } from "../";
+export default {
+  data() {
+    return {
+      workDuration: 5,
+      restDuration: 2,
+      wantsMaxWhenTimerDone: false,
+      wantsMinWhenTimerStart: false,
+    };
+  },
+};
 </script>
 <style lang=""></style>
