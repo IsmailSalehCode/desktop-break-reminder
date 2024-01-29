5e5ade286fd84f1bff09e7effb99d976ba8526e5
Ismail Saleh <centriu78@gmail.com>
2024-01-29 03:22:17 +0200

    isValidHexCode

--- src/rules-fields/rules-common.js
+++ src/rules-fields/rules-common.js
@@ -1,9 +1,11 @@
 const isPositiveWholeNumberRegex = /^\d+$/;
+const isHexRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
 
 const required = (v) => !!v || "Required.";
 const isPositiveWholeNumber = (v) =>
   isPositiveWholeNumberRegex.test(v) || "Positive whole numbers only.";
 const isLessThanMaxNumber = (v) =>
   v <= Number.MAX_SAFE_INTEGER || "Number exceeds max.";
+const isValidHexCode = (v) => isHexRegex.test(v) || "Invalid hex code.";
 
-export { required, isPositiveWholeNumber, isLessThanMaxNumber };
+export { required, isPositiveWholeNumber, isLessThanMaxNumber, isValidHexCode };
