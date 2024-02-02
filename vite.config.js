21cd3cc0d654e8b35e00ccb59b245cfdad5d5f2c
Ismail Saleh <centriu78@gmail.com>
2024-02-02 20:12:49 +0200

    so far so good

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,58 @@
-// Plugins
-import vue from "@vitejs/plugin-vue";
-import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+const Path = require("path");
+const vuePlugin = require("@vitejs/plugin-vue");
+import vuetify from "vite-plugin-vuetify";
 
-// Utilities
-import { defineConfig } from "vite";
-// import { fileURLToPath, URL } from "node:url";
+const { defineConfig } = require("vite");
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.ELECTRON == "true" ? "./" : ".",
+/**
+ * https://vitejs.dev/config
+ */
+const config = defineConfig({
+  root: Path.join(__dirname, "src", "renderer"),
+  publicDir: "public",
+  server: {
+    port: 8080,
+  },
+  open: false,
+  build: {
+    outDir: Path.join(__dirname, "build", "renderer"),
+    emptyOutDir: true,
+  },
   plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuePlugin(),
     vuetify({
       autoImport: true,
     }),
   ],
-  define: { "process.env": {} },
-  resolve: {
-    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
-  },
-  server: {
-    port: 3000,
-  },
 });
+
+module.exports = config;
+
+// // Plugins
+// import vue from "@vitejs/plugin-vue";
+// import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
+// // Utilities
+// import { defineConfig } from "vite";
+// // import { fileURLToPath, URL } from "node:url";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: process.env.ELECTRON == "true" ? "./" : ".",
+//   plugins: [
+//     vue({
+//       template: { transformAssetUrls },
+//     }),
+//     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+//     vuetify({
+//       autoImport: true,
+//     }),
+//   ],
+//   define: { "process.env": {} },
+//   resolve: {
+//     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
+//   },
+//   server: {
+//     port: 3000,
+//   },
+// });
