aee8d5a14e1c3d6341ae4648ac43db74241af092
Ismail Saleh <centriu78@gmail.com>
2024-02-02 20:34:20 +0200

    a

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,11 +1,8 @@
-// Components
+import { createApp } from "vue";
 import App from "./App.vue";
 
-// Composables
-import { createApp } from "vue";
 import store from "./store/storeTimer";
 
-// Plugins
 import { registerPlugins } from "./plugins";
 
 const app = createApp(App);
