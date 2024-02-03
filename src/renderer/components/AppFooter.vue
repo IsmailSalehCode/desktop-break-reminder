faac8439483b62622c018630e92e06a735499f5e
Ismail Saleh <centriu78@gmail.com>
2024-02-03 05:22:16 +0200

    ye

--- src/renderer/components/AppFooter.vue
+++ src/renderer/components/AppFooter.vue
@@ -5,16 +5,14 @@
         <v-col
           cols="12"
           style="
-            font-size: 0.8rem;
             text-align: center;
             text-wrap: balance;
             font-weight: bold;
-            letter-spacing: 3px;
+            letter-spacing: 2px;
           "
         >
           &#169; {{ yearCreated }} -
-          <a :href="linkMe" target="_blank">Исмаил Салех</a>
-          <!-- what to do with the bg text here TODO: should i add bg.pak to locales? electron-builder.json-->
+          <a :href="linkMe" target="_blank">Ismail Saleh</a>
         </v-col>
       </v-row>
     </v-col>
